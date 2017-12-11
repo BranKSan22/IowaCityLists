986dcba0b60e5b88ff6ff9a7d79b521cdf340631
BranKSan22 <32780020+BranKSan22@users.noreply.github.com>
2017-12-10 21:13:12 -0600

    Update account.sql

--- account.sql
+++ account.sql
@@ -5,11 +5,11 @@ CREATE TABLE account (
     id INT NOT NULL AUTO_INCREMENT,
     hashedpass VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-	email VARCHAR(300) NOT NULL,
-	location VARCHAR(255) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    persontype VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
 );
 
-INSERT INTO account (hashedpass, username, email, location) values ('$2a$12$s7h33TRR.wjhdKVdNIGON.pBVHlr2rancX6TxExL2UpLjTu.0TdbK', 'bksanders', 'brandy-sanders@uiowa.edu', 'Moline');
-INSERT INTO account (hashedpass, username, email, location) values ('$2a$12$4W4ej8SBkW6Z7wwEnG885OFemfYEPBTD1abWdg9ep0DqvZGc8LwyO','dwyrwas','duncan-wyrwas@uiowa.edu','Bettendorf');
-INSERT INTO account (hashedpass, username, email, location) values ('$2a$12$FqVuPRs7P3A3mlzneMnzC.FRUB6bLqk6mhGpQT00XY7kdD1.XZSuq','spandya', 'sanjana-pandya@uiowa.edu', 'Minnesota');                                             
+INSERT INTO account (hashedpass, username, email, persontype) values ('$2a$12$s7h33TRR.wjhdKVdNIGON.pBVHlr2rancX6TxExL2UpLjTu.0TdbK', 'bksanders', 'brandy-sanders@uiowa.edu', 'Student');
+INSERT INTO account (hashedpass, username, email, persontype) values ('$2a$12$4W4ej8SBkW6Z7wwEnG885OFemfYEPBTD1abWdg9ep0DqvZGc8LwyO','dwyrwas','duncan-wyrwas@uiowa.edu','Student');
+INSERT INTO account (hashedpass, username, email, persontype) values ('$2a$12$FqVuPRs7P3A3mlzneMnzC.FRUB6bLqk6mhGpQT00XY7kdD1.XZSuq','spandya', 'sanjana-pandya@uiowa.edu', 'Student');                                             
